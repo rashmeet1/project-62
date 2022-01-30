e8c816461f5d2860ffa676bba98e1ab689bedae8
rashmeet1 <76770021+rashmeet1@users.noreply.github.com>
2022-01-30 11:54:55 +0530

    Add files via upload

---
+++ localDb.js
@@ -0,0 +1,28 @@
+const db = {
+  "the": {
+		"chunks": ["th", "e"],
+		"phones": ["DH", "AH"]
+	},
+	"of": {
+		"chunks": ["o", "f"],
+		"phones": ["AH", "V"]
+	},
+	"and": {
+		"chunks": ["a", "n", "d"],
+		"phones": ["AH", "N", "D"]
+	},
+	"to": {
+		"chunks": ["t", "o"],
+		"phones": ["T", "UW"]
+	},
+	"a": {
+		"chunks": ["a"],
+		"phones": ["AH"]
+	},
+	"in": {
+		"chunks": ["i", "n"],
+		"phones": ["IH", "N"]
+	},
+}
+
+export default db;
